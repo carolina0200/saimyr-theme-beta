7fe11ec3f1c80c91263ab9f251e867bdf993568f
Carolina Giraldo <carolinagiraldo@saimyr.com.co>
2019-02-15 17:53:20 -0500

    Commit de emergencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'saimyr-library';
+export class AppComponent implements OnInit {
+  layout = 'saimyr-library';
+  constructor(private storage: StorageService) {}
+
+  ngOnInit() {
+    this.storage.setItem('layout', 'with-menu');
+    this.storage.watchStorage().subscribe(change => {
+      if (change) {
+        if (this.storage.getItem('layout') != null) {
+          this.layout = this.storage.getItem('layout');
+        }
+      }
+    });
+  }
 }
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  private storageSub = new Subject<boolean>();
+
+  watchStorage(): Observable<any> {
+    return this.storageSub.asObservable();
+  }
+
+  setItem(key: string, data: any) {
+    localStorage.setItem(key, data);
+    this.storageSub.next(true);
+  }
+
+  removeItem(key) {
+    localStorage.removeItem(key);
+    this.storageSub.next(true);
+  }
+
+  getItem(key): string {
+    return localStorage.getItem(key);
+  }
+
+}
